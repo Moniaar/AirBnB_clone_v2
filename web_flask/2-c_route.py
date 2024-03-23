3558f3f69629845a814200bb6d877a84b0b63267
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-23 13:35:23 +0200

    #6 A function to practise variables in a route in a flask application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This is a script that starts a Flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+	"""This method is to display a welcome message from the page that is hosted"""
+	return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def display():
+	"""This is a function to display the school name when sepcifying the route /hbnb"""
+	return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_C(text):
+	"""A method to display “C ” followed by the value of the text variable"""
+	text = text.replace("_", "")
+	return f'{C, {text}}'
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000)
+
