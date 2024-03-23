5cb4eabea76b768f21d4b626335f09c16f094b1a
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-23 13:09:56 +0200

    #5 New function /hbnb: display “HBNB”

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""This is a script that starts a Flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+	"""This method is to display a welcome message from the page that is hosted"""
+	return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def display():
+	"""This is a function to display the school name when sepcifying the route /hbnb"""
+	return "HBNB"
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000)
+
