54c47a34550d11fe59708cd8dc872a2656aa94fb
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-23 12:59:32 +0200

    #3 Fixed errors in the function def_hello

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is a script that starts a Flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+	"""This method is to display a welcome message from the page that is hosted"""
+	return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000)
