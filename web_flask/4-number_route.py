75db794624d0b3a4eb13aa22a9958e6781d28f60
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-24 03:32:16 +0200

    #18 Testin if you paased a number in the route of the website

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This is a script that starts a Flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """This method is to display a welcome message from
+    the page that is hosted"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display():
+    """This is a function to display the school name when
+    sepcifying the route /hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_C(text):
+    """A method to display “C ” followed by the value of
+    the text variable"""
+    text = text.replace("_", " ")
+    return 'C ' + text
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_py(text='is cool'):
+    """A method to display “python” followed by the value
+    of the text variable"""
+    text = text.replace("_", " ")
+    return 'Python ' + text
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def route_numb(n):
+    """This is script that starts a Flask web application with numbers"""
+    if n.isdigit():
+        return n + ' is a number'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
