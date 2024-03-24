702929f2598958dcd13c0c3166d3ec0ecb28fdbd
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-25 00:11:58 +0200

    #3 Fixed the reload method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -83,6 +83,14 @@ class DBStorage:
             self.__session.query(type(obj)).filter(
                     type(obj).id == obj.id).delete()
 
+    def reload(self):
+        """create all tables in the database"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
     def close(self):
         """Call remove methode on the private session attribute"""
-        self.reload
+        self.__session.close()
