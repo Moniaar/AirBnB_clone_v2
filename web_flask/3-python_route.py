0d157de42f6a8e15440585a925d495525ae4a14b
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-24 03:07:06 +0200

    #17 Still fixing pycode style

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,28 +9,34 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
-	"""This method is to display a welcome message from the page that is hosted"""
-	return "Hello HBNB!"
+    """This method is to display a welcome message from
+    the page that is hosted"""
+    return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def display():
-	"""This is a function to display the school name when sepcifying the route /hbnb"""
-	return "HBNB"
+    """This is a function to display the school name when
+    sepcifying the route /hbnb"""
+    return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def display_C(text):
-	"""A method to display “C ” followed by the value of the text variable"""
-	text = text.replace("_", " ")
-	return 'C' + text
+    """A method to display “C ” followed by the value of
+    the text variable"""
+    text = text.replace("_", " ")
+    return 'C ' + text
+
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def display_py(text='is cool'):
-	"""A method to display “python” followed by the value of the text variable"""
-	text = text.replace("_", " ")
-	return 'Python' + text
+    """A method to display “python” followed by the value
+    of the text variable"""
+    text = text.replace("_", " ")
+    return 'Python ' + text
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port=5000)
